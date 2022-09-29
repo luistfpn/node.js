0d38d8a0713bec446ce5cd7c00c6cb730970ab19
luistfpn <112507136+luistfpn@users.noreply.github.com>
2022-09-29 14:44:05 -0300

    exeercicio de operadores  logicos

--- operadores-logicos/app.js
+++ operadores-logicos/app.js
@@ -1,3 +1,5 @@
+const { read } = require('fs');
+
 const readline = require('readline').createInterface({
     input:process.stdin,
     output:process.stdout
@@ -7,5 +9,27 @@ console.log('Esse programa vai checar se voce e maior de 18 anos e tem habilitac
 console.log('Alem de suas verificacoes, precisamos verificar se voce esta na lista de presenca de horario');
 
 readline.question('qual o anos do seu nascimento?', ano=>{
-    
+        if(ano > 2004){
+            console.log('Voce nao tem 18 anos');
+        }else{
+            readline.question("Voce tem habilitaçao? (sim/nao)", temhabilitacao => { 
+                if(!(temhabilitacao.toUpperCase()==="SIM")){ 
+                    console.log('Voce nao tem habilitacao para entrar no kart');
+                }else{
+                    readline.question("Qual o seu nome?", nome => {
+                        switch(nome){ 
+                            case 'Douglas' :
+                                console.log('bem vindo ao kart Douglas');
+                                break;
+                            case 'Rafael' :
+                                console.log('bem vindo ao kart Rafael');
+                            default:
+                                console.log('Seu nome nao foi identificado na lista de presenca');
+                                
+                     }
+
+                 });     
+              }
+        })
+    }
 })
